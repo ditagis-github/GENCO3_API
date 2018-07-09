6f871db627d1dabd50eb957c630bd0d8cb524ab8
fuhidev <fuhi.dev@gmail.com>
2018-07-09 09:39:03 +0700

    Layer Infos

---
+++ WEB_API/Controllers/LayerInfoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataProvider;
+using DataProvider.Models;
+
+namespace WebAPI.Controllers
+{
+
+    [Authorize]
+    [RoutePrefix("api/layerinfo")]
+    public class LayerInfoController: ApiController
+    {
+        private AccountDB context = new AccountDB();
+        [Route("{id}")]
+        public HttpResponseMessage Get(string id)
+        {
+            try
+            {
+                var result = context.LayerInfos(new DataProvider.EF.SYS_Account
+                {
+                    Username = id
+                });
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception e)
+            {
+
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e.Message);
+            }
+        }
+    }
+}
